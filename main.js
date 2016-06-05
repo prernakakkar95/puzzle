1a6a0e47f985199bb1fc09797a553d7bffcbeee0
prerna kakkar <pkakkar95@gmail.com>
2016-06-05 11:35:28 +0530

    basic design

---
+++ main.js
@@ -0,0 +1,18 @@
+var init=(fuction(){
+const PUZZLE_DIFFICULTY = 4; //no of rows and columns of puzzle
+const PUZZLE_HOVER_TINT = '#ff0066';
+ 
+var _canvas; //
+var _stage;
+ 
+var _img;
+var _pieces;
+var _puzzleWidth;
+var _puzzleHeight;
+var _pieceWidth;
+var _pieceHeight;
+var _currentPiece;
+var _currentDropPiece;
+ 
+var _mouse;
+})
