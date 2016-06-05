992cabcb9043a675c5e27fcb8596d0daf11b206a
prerna kakkar <pkakkar95@gmail.com>
2016-06-05 12:04:59 +0530

    init

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var init=(fuction(){
 const PUZZLE_DIFFICULTY = 4; //no of rows and columns of puzzle
 const PUZZLE_HOVER_TINT = '#ff0066';
  
-var _canvas; //
+var _canvas; 
 var _stage;
  
 var _img;
@@ -14,5 +14,12 @@ var _pieceHeight;
 var _currentPiece;
 var _currentDropPiece;
  
-var _mouse;
+var _mouse; //to find out the current location of the cursor
+//to initiate the image building
+  
+       _img = new Image();
+       _img.addEventListener('load',onImage,false);
+       _img.src = "puzzle1.jpg";
+    
+	
 })
